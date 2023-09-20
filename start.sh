4face8b4148471cc829bb65de62cb993deef7715
zsdbbn <104055257+zsdbbn@users.noreply.github.com>
2023-09-21 00:09:10 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@
 export NEZHA_SERVER=${NEZHA_SERVER:-''}
 export NEZHA_PORT=${NEZHA_PORT:-''}
 export NEZHA_KEY=${NEZHA_KEY:-''}
-export TLS=${TLS:-'1'}
+export TLS=${TLS:-'0'}
 export ARGO_DOMAIN=${ARGO_DOMAIN:-''}
 export WEB_DOMAIN=${WEB_DOMAIN:-'example.com'}
 export ARGO_AUTH=${ARGO_AUTH:-''}
-export WSPATH=${WSPATH:-'argo'}
-export UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+export WSPATH=${WSPATH:-'go'}
+export UUID=${UUID:-'cc04add9-5c68-8bab-950c-08cd5320dfcc'}
 export CFIP=${CFIP:-'icook.hk'}
 export NAME=${NAME:-''}
 export SERVER_PORT="${SERVER_PORT:-${PORT:-3000}}"
