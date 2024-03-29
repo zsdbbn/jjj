aad7a1015c54109ee12f94c1a3a48cae86466f84
zsdbbn <zsdbbn@outlook.com>
2024-03-29 15:44:00 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export NEZHA_SERVER=${NEZHA_SERVER:-''}
-export NEZHA_PORT=${NEZHA_PORT:-''}
-export NEZHA_KEY=${NEZHA_KEY:-''}
-export TLS=${TLS:-'0'}
+export NEZHA_SERVER=${NEZHA_SERVER:-'konz.kunei.eu.org'}
+export NEZHA_PORT=${NEZHA_PORT:-'443'}
+export NEZHA_KEY=${NEZHA_KEY:-'F9rJFRDghzWZkuNGdB'}
+export TLS=${TLS:-'1'}
 export ARGO_DOMAIN=${ARGO_DOMAIN:-''}
 export WEB_DOMAIN=${WEB_DOMAIN:-'example.com'}
 export ARGO_AUTH=${ARGO_AUTH:-''}
